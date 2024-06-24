4ca4ec24ff9e568a6c2db249de3e7dfc6308e220
afaber999 <albert.faber@philips.com>
2024-06-25 01:27:22 +0200

    create dummy console.zig

--- kernel/console.zig
+++ kernel/console.zig
@@ -4,6 +4,11 @@ const SpinLock = @import("spinlock.zig").SpinLock;
 const Proc = @import("Proc.zig");
 const uart = @import("uart.zig");
 
+const c = @cImport({
+    @cInclude("c_funcs.h");
+});
+
+
 //
 // send one character to the uart.
 // called by printf(), and to echo input characters,
@@ -25,3 +30,26 @@ const uart = @import("uart.zig");
 //     @cInclude("spinlock.h");
 //     @cInclude("c_funcs.h");
 // });
+
+
+
+// TEMP WRAPPERS, TODO REMOVE WHEN POSSIBLE
+pub export fn zig_consputc(ch : c_int) void {
+    c.consputc(ch);
+}
+
+pub export fn zig_consolewrite(user_src : c_int, src : u64,  n:c_int) c_int {
+    return c.consolewrite(user_src, src, n);
+}
+
+pub export fn zig_consoleread(user_dst : c_int, dst : u64,  n:c_int) c_int {
+    return c.consoleread(user_dst, dst, n);
+}
+
+pub export fn zig_consoleintr(ch : c_int) void {
+    c.consoleintr(ch);
+}
+
+pub export fn zig_consoleinit() void {
+    c.consoleinit();
+}
