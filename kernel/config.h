cf2bce4a327f6412fb3c47f5a88b1fb4c3dbe68d
afaber999 <afaber999@github.com>
2024-08-18 11:52:17 +0200

    remove kernel.bin

--- kernel/config.h
+++ kernel/config.h
@@ -3,19 +3,46 @@
 #ifndef _CONFIG_H_
 #define _CONFIG_H_
 
-/* QEMU */
+#define THEAD_PTE
 
 #define NCPU          1  // maximum number of CPUs
 
-#define UART0 0x10000000UL
+#define UART0 0x04140000UL
 #define UART0_PHY UART0
-#define UART0_IRQ 10
-#define REG_SHIFT 0
+#define UART0_IRQ 44
+#define REG_SHIFT 2
 #define UART_CLK 25000000UL
-#define BAUD_RATE 230400UL
-#define PLIC 0x0c000000UL
+#define BAUD_RATE 115200UL
+#define PLIC 0x70000000UL
 #define PLIC_PHY PLIC
-#define VIRTIO0 0x10001000
-#define VIRTIO0_IRQ 1
+
+#define GPIO_DRIVER
+#define GPIO_NUM 4
+/* #define GPIO0 0x3020000 */
+/* #define GPIO1 0x3021000 */
+#define GPIO2 0x3022000
+/* #define GPIO3 0x3023000 */
+
+#define PWM_DRIVER
+#define PWM_NUM 4
+/* #define PWM0 0x3060000 */
+#define PWM1 0x3061000
+/* #define PWM2 0x3062000 */
+/* #define PWM3 0x3063000 */
+#define PWM_CLKSRC_FREQ 100000000UL
+
+#define ADC_DRIVER
+#define ADC_NUM 1
+#define ADC0 0x30f0000
+
+#define I2C_DRIVER
+#define I2C0 0x4000000
+
+#define SPI_DRIVER
+#define SPI0 0x41a0000
+#define SPI0_MAX_FREQ 500000
+#define SPI0_CLK_FREQ 100000000UL
+
+#define INTERVAL     250000UL
 
 #endif
