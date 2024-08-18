cea84ba7726bbbd0293333a09ba099506accb2fb
afaber999 <afaber999@github.com>
2024-08-18 12:31:14 +0200

    adapt build

--- build_for_milkv_duo.sh
+++ build_for_milkv_duo.sh
@@ -2,6 +2,11 @@ cp kernel/config.h.cv1800b kernel/config.h
 make
 make kernel/kernel.bin
 cp kernel/kernel.bin duo-imgtools/
-cd duo-imgtools
+pushd duo-imgtools
 mkimage -f xv6.its boot.sd
 ./genimage.sh -c xv6.cfg
+popd
+
+sync
+sudo dd if=duo-imgtools/milkv-duo_sdcard.img of=/dev/mmcblk0
+sync
